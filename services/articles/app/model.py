782956d739e6408edbe915dedb645b810dd13ea9
HaeckelK <haeckelk.github@gmail.com>
2021-08-18 19:21:24 +0100

    style: type hinting

--- services/articles/app/model.py
+++ services/articles/app/model.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class Article(db.Model):
+class Article(db.Model):  # type: ignore
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.Text, unique=True, nullable=False)
     date_added = db.Column(db.Integer, unique=False, nullable=False)
