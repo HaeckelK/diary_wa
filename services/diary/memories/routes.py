97ccb0c5ff0fdcf901858578739773f52982d323
HaeckelK <haeckelk.github@gmail.com>
2021-08-30 20:00:20 +0000

    wip: write memories to in memory list

--- services/diary/memories/routes.py
+++ services/diary/memories/routes.py
@@ -1,5 +1,25 @@
+from dataclasses import dataclass
+
+from flask import render_template, request, flash, redirect
+
 from diary.memories import bp
 
-@bp.route('/memories')
+@dataclass
+class Memory:
+    text: str
+
+
+DATA = [Memory("item1"), Memory("item2")]
+
+
+@bp.route('/memories', methods=["GET", "POST"])
 def index():
-    return "Memories"
+    recent_memories = DATA
+
+    if request.method == "POST":
+        DATA.append(Memory(request.form["content"]))
+        flash("Memory Added")
+        return redirect("/memories")
+
+
+    return render_template("memories/index.html", recent_memories=recent_memories)
