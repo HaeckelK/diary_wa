f5f8a53aced86abd9d45810ede2008d18aae63f7
HaeckelK <51128712+HaeckelK@users.noreply.github.com>
2021-09-02 17:52:41 +0100

    Connect to memories api (#53)
    * build: connect to docker network which has memories-api
    
    * feat: process response from memories API into Memory object for displaying on page
    
    * feat: remove 'Memory' text from add memories area
    
    * feat: connect form to make post to memories api
    
    Co-authored-by: HaeckelK <haeckelk.github@gmail.com>

--- services/diary/memories/routes.py
+++ services/diary/memories/routes.py
@@ -15,20 +15,25 @@ class Memory:
 @bp.route('/memories', methods=["GET", "POST"])
 def index():
     api_url = current_app.config["MEMORIES_API_URL"]
+
+    # TODO pass some limit to this request
     try:
         response = requests.get(api_url + "/memories")
     except ConnectionError:
-        recent_memories = []
+        raw_recent_memories = []
         flash("No connection to memories API.")
     else:
-        recent_memories = json.loads(response.content).values()
+        raw_recent_memories = json.loads(response.content).values()
+
+    recent_memories = []
+    for memory in raw_recent_memories:
+        recent_memories.append(Memory(text=memory["content"]))
 
     if request.method == "POST":
-        # TODO post to memories API
         content = request.form["content"]
         try:
             post_response = requests.post(api_url + "/memories",
-                                          data={"text": content})
+                                          data={"content": content})
         except ConnectionError:
             flash("Memory not added")
         else:
